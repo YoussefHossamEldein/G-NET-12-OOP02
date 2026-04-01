b430ebe4db6140235bf9458829e13e0d267f0052
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-02 00:08:38 +0200

    Question01

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            /* a) First problem : public fields so anyone from outside can access and change it directly */
+            /* second problem: no validation inside the methods on these fields */
+            // b) 
+             /* making private fields and use setters and getters with validation or use properties */
+             //example 
+            // internal class BankAccount
+            //    {
+
+            //private string owner;
+            //private double _balance;
+            //public double GetBalance()
+            //{
+            //    return _balance;
+            //}
+            //public void SetBalance (double amount)
+            //{
+            //    if(amount < 0)
+            //    {
+            //        Console.WriteLine("Balance can not be negative");
+            //        return;
+            //    }
+            //    _balance = amount;
+            //}
+            //// Using properties 
+            //    public double Balance
+            //       {
+            //          get;
+            //             set;
+            //       }
+            // C) because missuse is expected from user and unlogical use as well without validation of encapsulation
+
+
+
+                //}
+            #endregion
         }
     }
 }
