70a6697ee35052593a82984b0d20db5dfa71534c
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-08 00:10:04 +0200

    Movie Ticket Program

---
+++ Assignment02/Cinema.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTicketBooking
+{
+    public class Cinema
+    {
+        private Ticket[] _ticket = new Ticket[20];
+
+        public bool AddTicket(Ticket t)
+        {
+            for (int i = 0; i < _ticket.Length; i++)
+            {
+                if (_ticket[i] == null)
+                {
+                    _ticket[i] = t;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Ticket this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _ticket.Length)
+                {
+                    return _ticket[index];
+                }
+                return null;
+            }
+            set
+            {
+                if (index >= 0 && index < _ticket.Length)
+                {
+                    _ticket[index] = value;
+                }
+            }
+        }
+        public Ticket this[string moviename]
+        {
+            get
+            {
+                foreach (var movie in _ticket)
+                {
+                    if (movie.MovieName == moviename)
+                    {
+                        return movie;
+                    }
+                }
+                return null;
+            }
+
+        }
+    }
+}
--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Assignment02
+﻿using MovieTicketBooking;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Assignment02
 {
     internal class Program
     {
@@ -49,11 +53,72 @@
             /* public int Balance => balance > 0 ; */
             #endregion
 
+            #region Question03
+            /* this[int index] is called indexer and it the third way to violate the encapsulation 
+             * but it gives us the chance to treat objects from this class like a collection */
+            /* IndexOutOfRange Exception and i can handle to avoid this exception by validating 
+             * the input inside the indexer like if(index >= 0 && index < names.Length) so now 
+             * we can add our name */
+            #endregion
 
             #region Question04
             /* static keyword has made TotalOrders to be a class member not an instance(object) member */
             //No — static methods belong to the class, not an object. There's no instance to read the field from. You'd get a compile error.
             #endregion
+
+            #region Question05
+            bool isParsed;
+            TicketType tType;
+            char seatRow;
+            int seatNum;
+            double tPrice;
+            Cinema c = new Cinema();
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Please enter ticket {i} details : ");
+                Ticket t = new Ticket();
+                Console.Write("Enter Movie Name : ");
+                t.MovieName = Console.ReadLine();
+                do
+                {
+                    Console.Write("Enter Ticket Type : ");
+                    isParsed = Enum.TryParse<TicketType>(Console.ReadLine(), true, out tType);
+
+                } while (!isParsed || !(Enum.IsDefined(typeof(TicketType), tType)));
+                t.Type = tType;
+                do
+                {
+                    Console.Write("Enter Seat Row : ");
+                    isParsed = char.TryParse(Console.ReadLine(), out seatRow);
+                } while (!isParsed);
+                do
+                {
+                    Console.Write("Enter Seat number : ");
+                    isParsed = int.TryParse(Console.ReadLine(), out seatNum);
+                } while (!isParsed);
+                t.Seat = new Seat(seatRow, seatNum);
+                do
+                {
+                    Console.Write("Enter ticket price : ");
+                    isParsed = double.TryParse(Console.ReadLine(), out tPrice);
+                } while (!isParsed);
+                t.Price = tPrice;
+
+                c.AddTicket(t);
+            }
+            Console.WriteLine("========= All Tickets =========");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Ticket #{i} | {c[i].MovieName} | {c[i].Type} | {c[i].Seat} | {c[i].Price} | After Tax : {c[i].PriceAfterTax}");
+            }
+            Console.WriteLine("========= Search by Movie =======");
+            Console.Write("Enter movie name to search :  ");
+            string? movieName = Console.ReadLine();
+            Ticket FoundMovie = c[movieName];
+            Console.WriteLine($"Found Ticket {FoundMovie.TicketId} | {FoundMovie.MovieName} | {FoundMovie.Type} | {FoundMovie.Seat} | {FoundMovie.Price}");
+
+            #endregion
+
         }
     }
 }
